43cfbab3e69467e5684a4a38d173e87ffcda199a
Deviitt11 <98158844+Deviitt11@users.noreply.github.com>
2023-10-21 00:00:01 +0200

    Add files via upload

---
+++ Bingo.py
@@ -0,0 +1,8 @@
+# Dada una lista de números, esta devuelve "Bingo" si la lista contiene el número que introduce el usuario
+
+x = [11, 9, 47, 2, 97, 562, 6689, 444, 5, 97, 342] # Lista de números 
+
+num = int(input())
+
+if num in x:
+    print("Bingo");
