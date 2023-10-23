9454ec84df86c04673114cffb9f6596664098250
deviitt11 <david.collados11@gmail.com>
2023-10-24 00:36:08 +0200

    First Commit

---
+++ ListaAppend.py
@@ -0,0 +1,11 @@
+# Este es un ejemplo de uso de la función append
+
+# Hay una lista de nombres llamada nombres
+
+# Pedimos al usuario que introduzca otro nombre, este se añadirá a la lista y la lista resultante se imprimirá por pantalla
+
+names = ['Ana', 'Samuel', 'María', 'Pablo'] #Lista de nombres
+
+names.append(input("Introduce otro nombre: "));
+
+print(names)
