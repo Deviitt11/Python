1e56bd7c7b64cbf77b28e143bace023844986476
deviitt11 <david.collados11@gmail.com>
2023-10-24 00:38:16 +0200

    2nd Commit

--- ListaAppend.py
+++ ListaAppend.py
@@ -4,8 +4,8 @@
 
 # Pedimos al usuario que introduzca otro nombre, este se añadirá a la lista y la lista resultante se imprimirá por pantalla
 
-names = ['Ana', 'Samuel', 'María', 'Pablo'] #Lista de nombres
+names = ['Ana', 'Samuel', 'María', 'Pablo'] # Lista de nombres
 
-names.append(input("Introduce otro nombre: "));
+names.append(input("Introduce otro nombre: ")); # Mensaje para pedirle al usuario que introduzca otro nombre
 
-print(names)
+print(names) # Se imprime la lista por pantalla
