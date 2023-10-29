8b7f25349d470247fba604c964017bb63539a34c
deviitt11 <david.collados11@gmail.com>
2023-10-29 14:02:58 +0100

    Upload

---
+++ Bienvenida.py
@@ -0,0 +1,7 @@
+# Este programa da la bienvenida al usuario, la función toma su nombre como entrada y le imprime un mensaje de bienvenida
+
+def welcome(): 
+   name = input("Introduce tu nombre: ") # Pedimos al usuario que introduzca su nombre
+   print("Bienvenido,",name) # Esto es lo que imprimirá la función
+
+welcome() # Llamamos a la función
