d78da5b6b47eb77d8ca1aed1b9c7ed9eeb3db86e
deviitt11 <david.collados11@gmail.com>
2023-10-29 13:59:39 +0100

    Upload

---
+++ SumaLista.py
@@ -0,0 +1,10 @@
+# Este programa calcula la suma de una lista de números dada
+
+x = [74, 1, 24, 34, 110, 245, 5644, 24673, 7, 224, 93] # Lista de números
+
+total = 0; # Creamos una variable para almacenar el total
+
+for x in x: # Bucle for para ir sumando cada elemento
+    total += x;
+
+print("El total es: ", total); # Imprimimos por pantalla el total
