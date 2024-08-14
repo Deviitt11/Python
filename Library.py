5cddd3eafd8f233ee2d0a2a8dd252bc278cd2ebf
Deviitt11 <david.collados11@gmail.com>
2024-08-14 14:10:08 +0200

    Python Interm. - 14ago

---
+++ Library.py
@@ -0,0 +1,21 @@
+# Complete the Ebook class definition by properly initializing it 
+# to include the additional file_size attribute. 
+# Ensure that instances of Ebook inherit the title and author 
+# attributes from the Book class and also add the file_size 
+# attribute specific to ebooks.
+
+class Book:
+  def __init__(self, title, author):
+    self.title = title
+    self.author = author
+
+class Ebook(Book):
+  def __init__(self, title, author, file_size):
+  #complete the child class definition
+    super().__init__(title, author)
+    self.file_size = file_size
+
+my_ebook = Ebook("1984", "George Orwell", 10)
+print(my_ebook.title)
+print(my_ebook.author)
+print(my_ebook.file_size, 'MB')
